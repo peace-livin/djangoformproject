00e254bb2facef6eecd9fa3a1d3e57ba9753ea66
Akshay Darade <akshaydarade6675@gmail.com>
2025-06-30 19:31:45 +0000

    add employe form and add views  data in  teminal after form submit button

--- formapp/testapp/views.py
+++ formapp/testapp/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from testapp.form import EmployeeForm
 
 # Create your views here.
 def form(request):
-    
+    form = EmployeeForm()
+    if request.method == 'POST':
+        form = EmployeeForm(request.POST)
+        if form.is_valid():
+            print("Name is :",form.cleaned_data['ename'])
+            print("Salary is :",form.cleaned_data['esal'])
+            print("Address is :",form.cleaned_data['add'])
+            return render(request,'testapp/form.html',{'form':form})
+    return render(request,'testapp/form.html',{'form':form})
