0787366f02cfd64946e1844b3a6de7c15fc9e519
Akshay Darade <akshaydarade6675@gmail.com>
2025-06-30 19:47:38 +0000

    add validationerror for sumit button form

--- formapp/testapp/form.py
+++ formapp/testapp/form.py
@@ -1,6 +1,25 @@
 from django import forms
-
+from django.core.validators import*
 class EmployeeForm(forms.Form):
     ename = forms.CharField(max_length=100)
     esal = forms.IntegerField()
     add = forms.CharField(max_length=100)
+
+
+    def clean(self):
+        inputEname = self.cleaned_data.get('ename')
+        if len(inputEname)<12:
+            raise forms.ValidationError("Name should be atleast 12 characters")
+        return inputEname
+    
+    def clean_esal(self):
+        inputEsal = self.cleaned_data.get('esal')
+        if inputEsal<10000:
+            raise forms.ValidationError("Salary should be atleast 10000")
+        return inputEsal
+    
+    def clean_add(self):
+        inputAdd = self.cleaned_data.get('add')
+        if len(inputAdd)<10:
+            raise forms.ValidationError("Address should be atleast 12 characters")
+        return inputAdd
